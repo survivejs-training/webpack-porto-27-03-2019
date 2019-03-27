5844606075bb9e80d1f280a141b4463b0b25b828
Juho Vepsalainen <bebraw@gmail.com>
2019-03-27 16:23:12 +0000

    chore: Add a tree shaking demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,5 +2,8 @@ import "react";
 import "react-dom";
 import component from "./component";
 import "./main.css";
+import * as shake from "./shake";
+
+console.log(shake.bake);
 
 document.body.appendChild(component());
