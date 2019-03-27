ed7a72b0e5f0ca024a1b4982d164c732e74a272e
Juho Vepsalainen <bebraw@gmail.com>
2019-03-27 10:24:04 +0000

    chore: Format project

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,10 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+	mode: "production",
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Webpack demo"
+		})
+	]
 };
