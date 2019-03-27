9641be4a3fe719de717accf45d7fc1a48e85ead4
Juho Vepsalainen <bebraw@gmail.com>
2019-03-27 11:38:46 +0000

    chore: Set up wds

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -5,5 +5,21 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			title: "Webpack demo"
 		})
-	]
+	],
+
+	devServer: {
+		// Display only errors to reduce the amount of output.
+		stats: "errors-only",
+
+		// Parse host and port from env to allow customization.
+		//
+		// If you use Docker, Vagrant or Cloud9, set
+		// host: "0.0.0.0";
+		//
+		// 0.0.0.0 is available to all network devices
+		// unlike default `localhost`.
+		host: process.env.HOST, // Defaults to `localhost`
+		port: process.env.PORT, // Defaults to 8080
+		overlay: true
+	}
 };
